56fc8c37002cdbadecf0332e30b711ae52d1fa2a
TАИV! CH0UHАИ <83529262+tanvichouhan12@users.noreply.github.com>
2021-08-28 11:33:29 +0530

    Add files via upload

---
+++ TC003.CPP
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ char arr[2][3]={'1','2','3','4','5','6'};
+
+
+ for(int i=0;i<2;i++)
+ {               cout<<endl;
+ for(int j=0;j<3;j++)
+ {
+ cout<<arr[i][j];
+
+ }}
+ getch();
+ }
